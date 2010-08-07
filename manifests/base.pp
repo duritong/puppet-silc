e828c69b0b1ef65c19df88cd3ea1492ddb4f25ee
mh <mh@immerda.ch>
2010-08-07 02:12:26 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,23 +5,23 @@ class silc::base {
 
     file{
         '/etc/silcd/silcd.conf':
-            source => [ "puppet://$server/modules/site-silc/server/${fqdn}/silcd.conf",
-                        "puppet://$server/modules/site-silc/server/modules/silcd.conf",
-                        "puppet://$server/modules/silc/server/modules/silcd.conf" ],
+            source => [ "puppet:///modules/site-silc/server/${fqdn}/silcd.conf",
+                        "puppet:///modules/site-silc/server/modules/silcd.conf",
+                        "puppet:///modules/silc/server/modules/silcd.conf" ],
             notify => Service['silc-server'],
             require => Package['silc-server'],
             owner => silc, group => silc, mode => 0600;
         '/etc/silcd/silcalgs.conf':
-            source => [ "puppet://$server/modules/site-silc/server/${fqdn}/silcalgs.conf",
-                        "puppet://$server/modules/site-silc/server/modules/silcalgs.conf",
-                        "puppet://$server/modules/silc/server/modules/silcalgs.conf" ],
+            source => [ "puppet:///modules/site-silc/server/${fqdn}/silcalgs.conf",
+                        "puppet:///modules/site-silc/server/modules/silcalgs.conf",
+                        "puppet:///modules/silc/server/modules/silcalgs.conf" ],
             notify => Service['silc-server'],
             require => Package['silc-server'],
             owner => silc, group => silc, mode => 0600;
         '/etc/silcd/motd.txt':
-            source => [ "puppet://$server/modules/site-silc/server/${fqdn}/motd.txt",
-                        "puppet://$server/modules/site-silc/server/motd.txt",
-                        "puppet://$server/modules/silc/server/motd.txt" ],
+            source => [ "puppet:///modules/site-silc/server/${fqdn}/motd.txt",
+                        "puppet:///modules/site-silc/server/motd.txt",
+                        "puppet:///modules/silc/server/motd.txt" ],
             notify => Service['silc-server'],
             require => Package['silc-server'],
             owner => silc, group => silc, mode => 0600;
